3a4a88a0580b46ec3a65b88d927ffdd3e3a6e87b
james <jmosul@gmail.com>
2020-07-06 18:24:01 +0100

    feat: georgian locales

--- src/locales/en_us.ts
+++ src/locales/en_us.ts
@@ -1,12 +1,12 @@
 import Locale from '../types/Locale';
 
 const locale: Locale = {
-    name: 'common_us',
+    name: 'en_us',
     weekdays: 'Firstday_Secondday_Thirdday_Fourthday_Fifthday_Sixthday_Seventhday_Eighthday_Ninthday_Tenthday'.split('_'),
     weekdaysShort: '1st_Sec_Thi_For_Fif_Six_Sev_Eig_Nin_Ten'.split('_'),
     weekdaysMin: 'Fi_Se_Th_Fo_Fi_Si_Se_Ei_Ni_Te'.split('_'),
-    months: 'Deepwinter_Claw of Winter_Claw of Sunsets_Claw of Storms_Meltin_Time of Flowers_Summertide_Highsun_Fading_Leaffall_Rotting_Drawing Down'.split('_'),
-    monthsShort: 'Dwi_CWi_CSu_CSt_Mel_Flo_Sum_Hig_Fad_Lea_Rot_Dra'.split('_'),
+    months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
+    monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
     additionals: 'Midwinter_Greengrass_Midsummer_Highharvestide_Feast of the Moon_Shieldmeet'.split('_'),
     additionalsShort: 'MW_GG_MS_HH_FM_SM'.split('_'),
     era: 'Dale Reckoning',
